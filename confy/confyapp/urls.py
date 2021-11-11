b7ea1ddcee6db8fd908f4e66a27dddcf6ea2bd47
Ashok kumar <ashokkumar.201198@gmail.com>
2021-11-12 00:10:46 +0530

    added documentation in urls.py

--- confy/confyapp/urls.py
+++ confy/confyapp/urls.py
@@ -2,7 +2,14 @@ from django.urls import path
 from .views import *
 
 app_name = "confy"
-
+"""
+URLS: 
+o To create/edit a conference -> POST /api/v1/conference
+o To add/edit a talk -> POST /api/v1/talks
+o To add/remove speaker/participant from a talk. -> POST /api/v1/attendees for adding, DELETE /api/v1/attendees/<id> for removing
+o To list talks in a conference -> GET /api/v1/conferences/<conference_id>/talks
+o To list conferences -> GET /api/v1/conferences
+"""
 urlpatterns = [
     path('api/v1/conferences/', ConferenceApiView.as_view()),
     path('api/v1/conferences/<int:id>', ConferenceApiView.as_view()),
